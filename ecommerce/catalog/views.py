7d892fbd2d13b6c04167fc138ecba562be27e648
Júnior Carvalho <joseadolfojr@gmail.com>
2017-03-01 23:11:26 -0300

    templatetags

--- ecommerce/catalog/views.py
+++ ecommerce/catalog/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.views import generic
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
 
+    model = Product
+    template_name = 'catalog/product_list.html'
+    paginate_by = 3
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, 'catalog/category.html', context)
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
 
 
 def product(request, slug):
@@ -24,3 +30,6 @@ def product(request, slug):
         'product': product
     }
     return render(request, 'catalog/product.html', context)
+
+product_list = ProductListView.as_view()
+category = CategoryListView.as_view()
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+index = IndexView.as_view()
 
 
 def contact(request):
